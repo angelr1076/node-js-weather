55e664f73fa5678586eb7d732dd554f53a3dae29
Angel Rodriguez <angelr1076@gmail.com>
2019-03-22 10:03:47 -0400

    changed routes for Heroku push

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ weatherForm.addEventListener("submit", e => {
     return skycons.set(iconID, Skycons[currentIcon]);
   };
 
-  fetch(`http://localhost:3000/weather?address=${location}`)
+  fetch(`/weather?address=${location}`)
     .then(response => response.json())
     .then(data => {
       if (data.error) {
